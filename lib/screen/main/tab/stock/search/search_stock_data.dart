670bec3bdf164cdc842a1195cf96ab0403eeac8c
giyoungjang <janggy3355@gmail.com>
2024-08-27 19:48:36 +0900

    주식 검색화면 구조 만들기

---
+++ lib/screen/main/tab/stock/search/search_stock_data.dart
@@ -0,0 +1,25 @@
+import 'package:fast_app_base/common/data/preference/app_preferences.dart';
+import 'package:fast_app_base/screen/main/tab/stock/vo_simple_stock.dart';
+import 'package:get/get.dart';
+
+import '../../../../../common/util/local_json.dart';
+
+class SearchStockData extends GetxController {
+  List<SimpleStock> stocks = [];
+  RxList<String> searchHistoryList = <String>[].obs;
+  RxList<SimpleStock> autoCompleteList = <SimpleStock>[].obs;
+
+  @override
+  void onInit() {
+    searchHistoryList.addAll(['삼성전자', 'LG전자', '현대차', '넷플릭스']);
+    loadLocalStockJson();
+
+    super.onInit();
+  }
+
+  Future<void> loadLocalStockJson() async{
+    final jsonList = await LocalJson.getObjectList<SimpleStock>("json/stock_list.json");
+     stocks.addAll(jsonList);
+  }
+
+}
